19005709a3151439b4907ebaba42d1493c6463ff
Stigggggg <mikidrozd1@interia.pl>
2024-11-10 13:54:44 +0100

    Added quit button to checkers

---
+++ Lista 2/Checkers/Assets/Scripts/End.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class End : MonoBehaviour {
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
--- Lista 2/Checkers/Assets/Scripts/Menu.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Menu : MonoBehaviour {
-   public void GoToMenu() {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-   }
-}
